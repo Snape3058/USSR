a30956af96c343a54768caa47cb218fbb543f704
AlanSnape <alansnape@outlook.com>
2015-04-22 16:56:36 +0800

    basic structure

---
+++ main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python2
+#-*- encoding: utf-8 -*-
+
+import sys
+import thread
+import socket
+import time
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+cli = False
+try:
+    import mainForm
+except Exception as importException:
+    print importException
+    cli = True
+
+PORT = 23333
+BUFSIZE = 1024
+
+
+class FmainForm(QMainWindow, mainForm.Ui_Sender):
+
+    usingflag = False
+
+    def __init__(self, parent=None):
+        super(FmainForm, self).__init__(parent)
+        self.setupUi(self)
+        self.textEdit_send.setFocus()
+        self.connect(self.pushButton_sendmsg, SIGNAL(QString.fromUtf8("released()")), self.send)
+
+    def send(self):
+        udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        data = str(self.textEdit_send.toPlainText())
+        host = str(self.lineEdit.text())
+        while self.usingflag:
+            pass
+        self.usingflag = True
+        try:
+            udpsocket.sendto(data, (host, PORT))
+            udpsocket.close()
+            print "<<< " + str((data, (host, PORT)))
+            self.textEdit_history.append("<<< send to "+host+" :\n    "+data)
+            self.textEdit_history.append("")
+        except Exception as e:
+            print e
+        finally:
+            self.usingflag = False
+        time.sleep(0.1)
+
+    def receiveThread(self):
+        self.textEdit_history.append(" ")
+        udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udpsocket.bind(('', PORT))
+        while True:
+            try:
+                data, host = udpsocket.recvfrom(BUFSIZE)
+            except Exception as e:
+                print e
+            print ">>> " + str((data, host))
+            while self.usingflag:
+                pass
+            self.usingflag = True
+            self.textEdit_history.append(">>> received from "+host[0]+" :\n    "+data)
+            self.textEdit_history.append("")
+            self.usingflag = False
+        udpsocket.close()
+
+
+class cliForm:
+
+    host = "127.0.0.1"
+
+    def __init__(self):
+        thread.start_new_thread(self.receive, ())
+
+    def send(self):
+        instr = raw_input("["+self.host+"] ").split(':$:')
+        if 2 == len(instr):
+            self.host = instr[0]
+            data = instr[1]
+        else:
+            data = instr[0]
+        try:
+            udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udpsocket.sendto(data, (self.host, PORT))
+            udpsocket.close()
+        except Exception as e:
+            print e
+        print "<<< send to "+self.host+" :\n    "+data+"\n"
+
+    def receive(self):
+        udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udpsocket.bind(('', PORT))
+        while True:
+            try:
+                data, host = udpsocket.recvfrom(BUFSIZE)
+            except Exception as e:
+                print e
+            print "\n>>> received from "+host[0]+" :\n    "+data+"\n"
+
+if __name__ == '__main__':
+    if cli:
+        form_mainForm = cliForm()
+        while True:
+            form_mainForm.send()
+    else:
+        app = QApplication(sys.argv)
+        form_mainForm = FmainForm()
+        form_mainForm.show()
+        thread.start_new_thread(form_mainForm.receiveThread, ())
+        app.exec_()
---
+++ mainForm.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/mainForm.ui'
+#
+# Created: Wed Apr 22 13:40:58 2015
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Sender(object):
+    def setupUi(self, Sender):
+        Sender.setObjectName(_fromUtf8("Sender"))
+        Sender.resize(383, 584)
+        Sender.setMinimumSize(QtCore.QSize(383, 584))
+        Sender.setMaximumSize(QtCore.QSize(383, 584))
+        self.textEdit_history = QtGui.QTextEdit(Sender)
+        self.textEdit_history.setGeometry(QtCore.QRect(10, 10, 361, 391))
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Microsoft YaHei"))
+        font.setBold(True)
+        font.setWeight(75)
+        self.textEdit_history.setFont(font)
+        self.textEdit_history.setReadOnly(True)
+        self.textEdit_history.setObjectName(_fromUtf8("textEdit_history"))
+        self.textEdit_send = QtGui.QTextEdit(Sender)
+        self.textEdit_send.setGeometry(QtCore.QRect(10, 445, 361, 96))
+        self.textEdit_send.setObjectName(_fromUtf8("textEdit_send"))
+        self.pushButton_openfile = QtGui.QPushButton(Sender)
+        self.pushButton_openfile.setGeometry(QtCore.QRect(150, 550, 98, 27))
+        self.pushButton_openfile.setObjectName(_fromUtf8("pushButton_openfile"))
+        self.pushButton_sendmsg = QtGui.QPushButton(Sender)
+        self.pushButton_sendmsg.setGeometry(QtCore.QRect(270, 550, 98, 27))
+        self.pushButton_sendmsg.setObjectName(_fromUtf8("pushButton_sendmsg"))
+        self.label = QtGui.QLabel(Sender)
+        self.label.setGeometry(QtCore.QRect(10, 415, 66, 17))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.lineEdit = QtGui.QLineEdit(Sender)
+        self.lineEdit.setGeometry(QtCore.QRect(80, 410, 291, 27))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+
+        self.retranslateUi(Sender)
+        QtCore.QMetaObject.connectSlotsByName(Sender)
+
+    def retranslateUi(self, Sender):
+        Sender.setWindowTitle(_translate("Sender", "Dialog", None))
+        self.pushButton_openfile.setText(_translate("Sender", "Send file", None))
+        self.pushButton_sendmsg.setText(_translate("Sender", "Send Text", None))
+        self.label.setText(_translate("Sender", "Send to: ", None))
+        self.lineEdit.setText(_translate("Sender", "127.0.0.1", None))
+
